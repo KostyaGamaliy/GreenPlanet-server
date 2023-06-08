25f7bb44fcfa9bac167e5080ef98b53d7972eddc
Kostya Gamaliy <kostiantyn.hamalii@nure.ua>
2023-06-08 16:51:05 +0300

    login-register-work

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,71 +1,72 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+    namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginRequest;
-use App\Http\Requests\RegisterRequest;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+    use App\Http\Controllers\Controller;
+    use App\Http\Requests\LoginRequest;
+    use App\Http\Requests\RegisterRequest;
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller
-{
-    public function login(LoginRequest $request)
+    class AuthController extends Controller
     {
-        $credentials = request(['email', 'password']);
-        if (!auth()->attempt($credentials)) {
-            return response()->json([
-                'message' => 'The given data was invalid',
-                'errors' => [
-                    'password' => [
-                        'Invalid credentials'
+        public function login(LoginRequest $request)
+        {
+            $credentials = request(['email', 'password']);
+            if (!auth()->attempt($credentials)) {
+                return response()->json([
+                    'message' => 'The given data was invalid',
+                    'errors' => [
+                        'password' => [
+                            'Invalid credentials'
+                        ]
                     ]
-                ]
-            ], 422);
+                ], 422);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            $authToken = $user->createToken('auth-token')->plainTextToken;
+            $user->update(['login_token' => $authToken]);
+
+            return response()->json([
+                'user' => $user,
+                'access_token' => $authToken
+            ]);
         }
 
-        $user = User::where('email', $request->email)->first();
-        $authToken = $user->createToken('auth-token')->plainTextToken;
-        $user->update(['login_token' => $authToken]);
+        public function register(RegisterRequest $request)
+        {
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+                $data['image'] = $image->store('images', 'public');
+            } else {
+                $data['image'] = 'images/default-image-for-user.png';
+            }
 
-        return response()->json([
-           'access_token' => $authToken
-        ]);
-    }
+            $user = User::create([
+                'email' => $request->email,
+                'full_name' => $request->full_name,
+                'role_id' => $request->role_id,
+                'image' => $data['image'],
+                'password' => bcrypt($request->password)
+            ]);
 
-    public function register(RegisterRequest $request)
-    {
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $data['image'] = $image->store('images', 'public');
-        } else {
-            $data['image'] = 'images/default-image-for-user.png';
+            return response()->json($user);
         }
 
-        $user = User::create([
-            'email' => $request->email,
-            'full_name' => $request->full_name,
-            'role_id' => $request->role_id,
-            'image' => $data['image'],
-            'password' => bcrypt($request->password)
-        ]);
+        public function logout(Request $request)
+        {
+            $user = Auth::guard('sanctum')->user();
 
-        return response()->json($user);
-    }
+            if ($user) {
+                $user->tokens()->where('id', $request->user()->currentAccessToken()->id)->delete();
+                $user->login_token = null;
+                $user->save();
+            }
 
-    public function logout(Request $request)
-    {
-        $user = Auth::guard('sanctum')->user();
-
-        if ($user) {
-            $user->tokens()->where('id', $request->user()->currentAccessToken()->id)->delete();
-            $user->login_token = null;
-            $user->save();
+            return response()->json([
+                'message' => 'Logged out successfully'
+            ]);
         }
-
-        return response()->json([
-            'message' => 'Logged out successfully'
-        ]);
     }
-}
