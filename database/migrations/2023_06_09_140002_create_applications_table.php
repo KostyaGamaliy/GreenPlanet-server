c25027275d05c94bea31269c9963f7575d830725
Kostya Gamaliy <kostiantyn.hamalii@nure.ua>
2023-06-09 17:06:14 +0300

    applications-migrate

---
+++ database/migrations/2023_06_09_140002_create_applications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sender_id');
+            $table->string('company_name');
+            $table->string('company_description');
+            $table->string('company_image');
+            $table->string('documents');
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
